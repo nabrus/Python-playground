9d18dea2f7b5a0abad1de270d576230825bec5f5
butters <34988577+nabrus@users.noreply.github.com>
2023-04-15 00:27:00 -0400

    Complete first part of lesson

--- blackjack/main.py
+++ blackjack/main.py
@@ -1,3 +1,8 @@
-suit = "hearts"
+suits = ["spades", "clubs", "hearts", "diamonds"]
+suit = suits[2]
 rank = "K"
 value = 10
+print(f"Your card is: {rank} of {suit}")
+suits.append("snakes")
+for suit in suits:
+    print(suit)
